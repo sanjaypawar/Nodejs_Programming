1d379b89950f028358c8d6e4230eb9e314cb2c8d
sanjaypawar <sanjaybpawar@gmail.com>
2018-12-20 13:39:02 +0530

    First Nodejs Prg

---
+++ Node.js
@@ -0,0 +1,8 @@
+http.createServer(function (request, response) {
+   
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+     response.end('Hello World\n');
+}).listen(8000);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8000/');
